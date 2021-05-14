a50c67938bcd71827fd0e57cdb65c12165f8803b
GiorgosKoulouris <grgkoulouris@yahoo.gr>
2021-05-14 03:51:52 +0300

    Added basic GUI elements and functionality.
    Squash merge of:
        Added links to branches (README.md)
        Minor code cleanup
        Updated README.md file
        Added plotGainButton and implemented its functionality
        Implemented startTime and endTime functionality
        Added Labels and Entry Boxes relative to user's choice about start/end time
        Made the script run as an app and added test GUI elements and functions

--- README.md
+++ README.md
@@ -1,17 +1,50 @@
-# pythonAudioHelper
+# AudioProcessor Visualiser 
+![GitHub top language](https://img.shields.io/github/languages/top/GiorgosKoulouris/audioProcessorVisualiser) 
+![GitHub last commit](https://img.shields.io/github/last-commit/GiorgosKoulouris/audioProcessorVisualiser)
 
+[comment]: <> (![GitHub commit activity]&#40;https://img.shields.io/github/commit-activity/w/GiorgosKoulouris/audioProcessorVisualiser&#41;)
 
-![GitHub top language](https://img.shields.io/github/languages/top/GiorgosKoulouris/pythonAudioHelper)
-![GitHub last commit](https://img.shields.io/github/last-commit/GiorgosKoulouris/pythonAudioHelper)
-<!-- ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/GiorgosKoulouris/pythonAudioHelper) -->
 ***
 ## Description
-pythonAudio helper is a python program that visualises DSP code behaviour
+AudioProcessor Visualiser is an under construction open-source app that has the intention of visualising the impact that
+a block of DSP code has on a given audio signal. User will be able to load a local audio file and process it 
+(or a part of it) using his own implementation of the code that will do the actual signal processing. After that, he 
+will be able to see the impact of the code regarding its frequency response and energy/amplitude footprint.
 
-## Download
-To either to customize or use it just clone this repo and after creating your venv run:
+___
 
-`python -m pip install -r Requirements.txt`
+### Languages and Libraries
+AudioProcessor Visualiser is a project coded using entirely Python. It currently depends on high-level libraries such as
+[librosa](https://librosa.org/) and [matplotlib](https://matplotlib.org/) for functions such as loading audio files and
+"plotting".
+
+___
+
+### Download
+
+Although the project is in an early stage, feel free to use or manipulate the app. Just clone the repo and install all 
+the necessary dependencies using 
+
+`python -m pip install -r Requirements.txt` 
+
+If you install the dependencies in an environment with wheel installed, an empty pip cache directory is suggested:
+
+`rm -rf $(pip cache dir)` and then `python -m pip install -r Requirements.txt`
+
+
+---
+
+###Contributing
+
+Please, keep in mind that I started the AudioProcessor Visualiser project (as well as learning Python) as a side project 
+to help myself fine tune my C++ DSP coding. Plus, I have minimal experience in the fields of Software and Computer Engineering. 
+So please feel free to contribute either by proposing changes and features (by commenting or via a pull request), or just 
+by commenting out bad code craftsmanship!!
+
+I mainly post my progress on the [dev](https://github.com/GiorgosKoulouris/audioProcessorVisualiser/tree/dev) branch. 
+Although I make regular merges to the [main](https://github.com/GiorgosKoulouris/audioProcessorVisualiser/tree/main), 
+in case of pull requests make sure to rebase against [dev](https://github.com/GiorgosKoulouris/audioProcessorVisualiser/tree/dev)
+before submitting the pull request.
 
 ***
 
--- Source/main.py
+++ Source/main.py
@@ -1,37 +1,75 @@
+"""
+================================== USAGE ===============================
+
+Intended Usage (with implemented functionality):
+    1. Choose start and end times (in secs) and click import to load the file
+    2. Click the Plot Gain button to show a plot of Gain over Time
+        --> checking the checkbutton on the left also plots the original and processed signal
+
+===============================================================================
+"""
+
+import tkinter
 import librosa as lr
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import Tk
+from tkinter.ttk import Button, Label, Entry, Checkbutton
+from tkinter.filedialog import askopenfilename
 
 # ================ Global Variables =====================
+# Path
+fileImported = False
+importPath = "~/Desktop"
 
-# ATM this is a const that points to a local audio file in my system
-importPath = "/Users/cliff/Sirius/2.Projects/DOZEN/Stems/Vigil/Masters/Dozen_VIgil.wav"
-
+# File and Signal related
 sampleRate = 44100
-inSignal = np.zeros(sampleRate)
-durationInSecs = 3
-outSignal = inSignal.copy
+convertToMono = False
+inputS = np.zeros(sampleRate)
+startTime = 0
+endTime = 4
+durationInSecs = endTime - startTime
+output = inputS.copy
+
+# Plotting options
+includeWavesInGainPlot = False
 
 # =================== Function Definitions ============
 # Import an audio file
 def importFile():
     """Import the selected file"""
-    global importPath
-    global sampleRate
-    global inSignal
-    global durationInSecs
-    global outSignal
-    inSignal, sampleRate = lr.load(importPath, sr=None, offset=20, mono=True, duration=durationInSecs)
-    outSignal = inSignal.copy
+    rangeIsCorrect = ((int(startTime.get())) >= 0) and ((int(endTime.get()) > int(startTime.get())))
+    if rangeIsCorrect:
+        tempPath = askopenfilename()
+        if tempPath.endswith(".wav"):
+            global importPath
+            global sampleRate
+            global inputS
+            global durationInSecs
+            global output
 
-# You should copy your actual audio processing code in here
+            importPath = tempPath
+
+            durationInSecs = int(endTime.get()) - int(startTime.get())
+            inputS, sampleRate = lr.load(importPath, sr=None, offset=20, mono=True, duration=durationInSecs)
+            output = processAudio(inputS)
+
+            global fileImported
+            fileImported = True
+
+# User will actually override this function
 def processAudio(signal):
-    """Processes the whole loaded audio file"""
-    # Hard-clips the signal @ threshold and starts increasing when gain[x] > gain[x-1] (even if > threshold)
+    """Processes the section of the audio file that was loaded"""
+    # Sample code, for demonstration purposes
 
-    # User Input
+    """
+    Hard-clips the signal @ threshold and starts increasing  gain when 
+    gain[x] > gain[x-1] (even if > threshold)
+    """
+
+    # User inputS
     thresholdInGain = .7
-    release = 50
+    release = 250
 
     # Calculations
     releaseDelta = 1 / ((release / 1000) * sampleRate)
@@ -49,11 +87,11 @@ def processAudio(signal):
             else:
                 if out[sample] > 0:
                     out[sample] = thresholdInGain
-                    gain = out[sample] / inSignal[sample]
+                    gain = out[sample] / inputS[sample]
                     maxGainR = 1 - gain
                 else:
                     out[sample] = -thresholdInGain
-                    gain = out[sample] / inSignal[sample]
+                    gain = out[sample] / inputS[sample]
                     maxGainR = 1 - gain
         else:
             if (previousGain + (maxGainR * releaseDelta)) < 1:
@@ -65,47 +103,102 @@ def processAudio(signal):
             previousGain = gain
     return out
 
+# ================= Plotting functions ======================
+
 # Plot single waveform
 def plotWaveform(signal, label):
-    x = np.linspace(0, durationInSecs, len(inSignal))
-    plt.plot(x, signal, color='b', label=label)
-    plt.show()
+    x = np.linspace(0, durationInSecs, len(inputS))
+    fig, ax = plt.subplots(1, 1, num="Single Waveform")
+    ax.plot(x, signal, color='g', label=label)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Sample value")
+    fig.show()
 
-# Plot both input and output
+# Plot both inputS and output
 def plotStackedWaveforms():
-    x = np.linspace(0, durationInSecs, len(inSignal))
-    plt.plot(x, inSignal, color='g', label='Input')
-    plt.plot(x, outSignal, color='b', label='Output')
-    plt.show()
+    x = np.linspace(0, durationInSecs, len(inputS))
+    fig, ax = plt.subplots(1, 1, num="Stacked Waveforms")
+    ax.plot(x, inputS, color='g', label='inputS')
+    ax.plot(x, output, color='b', label='Output')
+
+    fig.show()
+
+# Function for includeWavesInGainPlotButton
+def setIncludeWavesInGain():
+    global includeWavesInGainPlot
+    if includeWavesInGainPlot:
+        includeWavesInGainPlot = False
+    else:
+        includeWavesInGainPlot = True
 
 # Function that calculates gain reduction applied to the signal
-def plotGain(alsoPlotWaveforms: bool):
+def plotGain():
     """This function plots the gain reduction over time"""
-    """If <alsoPlotWaveforms = True>, it also plots a stacked overview of the waveforms"""
-    g = outSignal / inSignal
-    x = np.linspace(0, durationInSecs, len(inSignal))
+    """If <includeWavesInGainPlot = True>, it also plots a stacked overview of the waveforms"""
+    if fileImported:
+        g = np.zeros(len(inputS))
 
-    if alsoPlotWaveforms:
-        fig, ax = plt.subplots(2, 1, sharex=True, num="Dynamics")
-        ax[0].plot(x, g, color='r', label='Gain Reduction')
-        ax[0].set_ylabel("Gain value")
-        ax[1].plot(x, inSignal, color='g', label='y1')
-        ax[1].plot(x, outSignal, color='b', label='y2')
-        plt.xlabel("Time (s)")
+        # Quick fix to avoid division issues. Needs fixups <------
+        for sample in range(len(inputS)):
+            if inputS[sample] != 0:
+                g[sample] = output[sample] / inputS[sample]
+            else:
+                g[sample] = g[sample - 1]
 
-    else:
-        fig, ax = plt.subplots(1, 1, num="Dynamics")
-        ax.plot(x, g, color='r', label='Gain Reduction')
+        x = np.linspace(0, durationInSecs, len(inputS))
 
-    plt.xlabel("Time (s)")
-    plt.ylabel("Sample Value")
-    plt.legend()
-    plt.show()
+        if includeWavesInGainPlot:
+            fig, ax = plt.subplots(2, 1, sharex=True, num="Gain Over Time")
+            ax[0].plot(x, g, color='r', label='Gain Reduction')
+            ax[0].set_ylabel("Gain value")
+            ax[1].plot(x, inputS, color='g', label='y1')
+            ax[1].plot(x, output, color='b', label='y2')
+            ax[1].set_ylabel("Sample value")
+            plt.xlabel("Time (s)")
+
+        else:
+            fig, ax = plt.subplots(1, 1, num="Gain Over Time")
+            ax.plot(x, g, color='r', label='Gain')
+            plt.xlabel("Time (s)")
+            plt.ylabel("Gain Value")
+
+        plt.legend()
+        plt.show()
+
+# This function initialises all GUI elements
+def initUI(window: Tk):
+    window.geometry('600x600')
+    window.title('DSP Visualiser')
+
+    global startTime
+    startTime = tkinter.StringVar(window)
+    startTLabel = Label(window, text='start', justify='center')
+    startTLabel.place(x=10, y=10, width=40, height=30)
+    startTBox = Entry(textvariable=startTime)
+    startTBox.place(x=55, y=10, width=35, height=30)
+
+    global endTime
+    endTime = tkinter.StringVar(window)
+    endTLabel = Label(window, text='end', justify='center')
+    endTLabel.place(x=100, y=10, width=30, height=30)
+    endTBox = Entry(textvariable=endTime)
+    endTBox.place(x=135, y=10, width=35, height=30)
+
+    importFileButton = Button(window, text='Import', command=importFile)
+    importFileButton.place(x=500, y=10, width=75, height=30)
+
+    global includeWavesInGainPlot
+    includeWavesInGainPlot = False
+    wavesInGainButton = Checkbutton(variable=includeWavesInGainPlot, command=setIncludeWavesInGain)
+    wavesInGainButton.place(x=50, y=50, width=35, height=35)
+
+    plotGainButton = Button(window, text='Plot gain over time', command=plotGain)
+    plotGainButton.place(x=120, y=50, width=200, height=35)
 
 
-# ========= Execution ==========
-importFile()
-outSignal = processAudio(inSignal)
-# plotWaveform(outSignal, "in")
-# plotStackedWaveforms()
-plotGain(False)
+# ========== App loop =============
+mainWindow = Tk()
+initUI(mainWindow)
+
+mainWindow.mainloop()
+
