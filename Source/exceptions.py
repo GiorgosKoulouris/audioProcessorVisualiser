d6e5480b49c8a4124e3817244303a545fe09614e
GiorgosKoulouris <grgkoulouris@yahoo.gr>
2021-05-22 17:15:54 +0300

    Changed the custom exception class behaviour

--- Source/exceptions.py
+++ Source/exceptions.py
@@ -1,8 +1,31 @@
 class FilePathException(Exception):
-    pass
+    """Custom exception raised when an undesired file path appears"""
+
+    def __init__(self, message, error=None):
+        self.message = message
+        self.error = error
+
+    def __str__(self):
+        return (f'FilePathException: {self.message}')
+
 
 class UserInputExeption(Exception):
-    pass      
+    """Custom exception raised when user inputs values that are not allowed or are meaningless"""
+
+    def __init__(self, message, error=None):
+        self.message = message
+        self.error = error
+
+    def __str__(self):
+        return (f'UserInputExeption: {self.message}')
+
 
 class ScriptReturnCodeException(Exception):
-    pass
+    """Custom exception raised when scripts return failure values"""
+
+    def __init__(self, message, error=None):
+        self.message = message
+        self.error = error
+
+    def __str__(self):
+        return (f'ScriptReturnCodeException: {self.message}')
